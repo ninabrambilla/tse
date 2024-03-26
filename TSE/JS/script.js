bf0b0f86f2c048a8e9a8fdebfc5a6663e7abe776
Marina <127635780+ninabrambilla@users.noreply.github.com>
2024-03-26 15:47:57 -0300

    Add files via upload

---
+++ TSE/JS/script.js
@@ -0,0 +1,9 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("sliderValue");
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+
+// Atualizar o valor inicial
+output.innerHTML = slider.value;
