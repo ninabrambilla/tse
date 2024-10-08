7aefa9cf0e5622f5e978b937f6323540eec87253
Marina Brambilla <127635780+ninabrambilla@users.noreply.github.com>
2024-10-08 13:21:43 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,14 +1,9 @@
 <?php
 
- $dbHost = 'Localhost';
- $dbUsername = 'root';
- $dbPassword = '';
- $dbName = 'tse';
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'tse';
 
- $conecta = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
-
- $sql = mysqli_query($conecta, "SELECT * FROM cadastro");
-
- echo "Existem " .mysqli_num_rows($sql). " resgistros.";
-
+$conecta = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 ?>
